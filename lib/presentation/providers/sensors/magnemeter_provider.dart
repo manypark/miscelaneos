f6d324fb4d8bafb49d2406eeb908d274bee79338
Manu <manypark@live.com>
2023-08-19 11:36:28 -0600

    feat: magnetometer implemented

---
+++ lib/presentation/providers/sensors/magnemeter_provider.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sensors_plus/sensors_plus.dart';
+
+class MagnemeterXYZ {
+  
+  final double x;
+  final double y;
+  final double z;
+
+  MagnemeterXYZ(this.x, this.y, this.z);
+
+  @override
+  String toString() {
+    return '''
+      x: $x,
+      y: $y,
+      z: $z 
+    ''';
+  }
+}
+
+final magnetometerProvider = StreamProvider.autoDispose<MagnemeterXYZ>((ref) async* {
+
+  await for( final event in magnetometerEvents ) {
+
+    final x = double.parse( (event.x.toStringAsFixed(2)) );
+    final y = double.parse( (event.y.toStringAsFixed(2)) );
+    final z = double.parse( (event.z.toStringAsFixed(2)) );
+
+    yield MagnemeterXYZ(x, y, z);
+  }
+  
+});
--- lib/presentation/screens/sensors/magnometer_screen.dart
+++ lib/presentation/screens/sensors/magnometer_screen.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:miscelaneos/presentation/providers/sensors/magnemeter_provider.dart';
 
-class MagnometerScreen extends StatelessWidget {
+class MagnometerScreen extends ConsumerWidget {
 
   const MagnometerScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
+
+    final magnemeter$ = ref.watch( magnetometerProvider );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Magnometro'),
       ),
-      body: const Center(
-        child: Text('Hola manu'),
+      body: Center(
+        child:  magnemeter$.when(
+          data    : (data) => Text(data.toString()), 
+          error   : (error, _) => Text('$error'), 
+          loading : () => const CircularProgressIndicator(),
+        )
       ),
     );
   }
