9beedfa19cab42286c103535836a51a75e276a18
Manu <manypark@live.com>
2023-08-19 12:43:39 -0600

    feat: animation for needle

--- lib/presentation/screens/sensors/compass_screen.dart
+++ lib/presentation/screens/sensors/compass_screen.dart
@@ -40,11 +40,42 @@ class CompassScreen extends ConsumerWidget {
 
 }
 
-class Compass extends StatelessWidget {
+class Compass extends StatefulWidget {
 
   final double heading;
 
   const Compass({super.key, required this.heading});
+
+  @override
+  State<Compass> createState() => _CompassState();
+}
+
+class _CompassState extends State<Compass> {
+
+  double prevValue = 0.0;
+  double turns = 0;
+
+  double getTurns() {
+
+    double? direction = widget.heading;
+    direction = (direction < 0) ? (360 + direction): direction;
+
+    double diff = direction - prevValue;
+    if(diff.abs() > 180) {
+
+      if(prevValue > direction) {
+        diff = 360 - (direction-prevValue).abs();
+      } else {
+        diff = 360 - (prevValue-direction).abs();
+        diff = diff * -1;
+      }
+    }
+
+    turns += (diff / 360);
+    prevValue = direction;
+
+    return turns * -1;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +84,7 @@ class Compass extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.center,
       children          : [
 
-        Text('${heading.ceil()}', style: const TextStyle( color: Colors.white, fontSize: 30 ),),
+        Text('${widget.heading.ceil()}', style: const TextStyle( color: Colors.white, fontSize: 30 ),),
         const SizedBox( height: 20,),
 
         Stack(
@@ -61,13 +92,20 @@ class Compass extends StatelessWidget {
           children  : [
 
             Transform.rotate(
-              angle:  ( heading * ( pi / 180 ) * -1),
+              angle:  ( widget.heading * ( pi / 180 ) * -1),
               child: Image.asset('assets/images/compass/quadrant-1.png'),
             ),
 
-            Transform.rotate(
-              angle:  ( heading * ( pi / 180 ) * -1),
-              child: Image.asset('assets/images/compass/needle-1.png'),
+            // Transform.rotate(
+            //   angle:  ( widget.heading * ( pi / 180 ) * -1),
+            //   child: Image.asset('assets/images/compass/needle-1.png'),
+            // ),
+
+            AnimatedRotation(
+              turns   : getTurns(),
+              duration: const Duration( seconds: 1),
+              curve   : Curves.easeOut,
+              child   : Image.asset('assets/images/compass/needle-1.png'),
             ),
 
           ],
@@ -75,5 +113,4 @@ class Compass extends StatelessWidget {
       ],
     );
   }
-
 }
